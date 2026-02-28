aeb21475760104870546ffcdcb4f0b9e70709fce
Mark Farnsworth <farnsworth2008@gmail.com>
2026-02-28 12:47:50 -0500

    feat: wip
    Issue: main

--- tests.go
+++ tests.go
@@ -47,7 +47,7 @@ func testNewFile01(t *testing.T, parser hcl.HclParser) {
 	expect(t, strconv.Itoa(eLen), "1")
 	elements = elements[0].NestedElements()
 	eLen = len(elements)
-	expect(t, strconv.Itoa(eLen), "10")
+	expect(t, strconv.Itoa(eLen), "17")
 
 	// Build the string we need for our expectation
 	b := []string{}
@@ -55,19 +55,25 @@ func testNewFile01(t *testing.T, parser hcl.HclParser) {
 	b = append(b, "# lines, null values, and multi-line strings. The parser should be able to")
 	b = append(b, "# handle all of these elements correctly and produce the expected output.")
 	b = append(b, "#")
-	b = append(b, "# This file should have 10 elements.")
+	b = append(b, "# This file should have 17 elements.")
 	b = append(b, "#")
 	b = append(b, "# [0]    - the file block")
 	b = append(b, "# [0][0] - the multi-line comment")
-	b = append(b, "# [0][1] - a pair with a null value")
-	b = append(b, "# [0][2] - a pair with a null and without whitespace around the equals sign")
-	b = append(b, "# [0][3] - a pair with a null and whitespace on right of equals sign")
+	b = append(b, "# [0][1] - a pair with a null")
+	b = append(b, "# [0][2] - a pair with a null without whitespace")
+	b = append(b, "# [0][3] - a pair with a null with whitespace on the right")
 	b = append(b, "# [0][4] - a blank line")
-	b = append(b, "# [0][5] - a pair where the value is a string")
-	b = append(b, "# [0][6] - another blank line")
-	b = append(b, "# [0][7] - a pair with a long name and another null value")
-	b = append(b, "# [0][8] - yet another blank line")
-	b = append(b, "# [0][9] - a pair with a multi line string")
+	b = append(b, "# [0][5] - a pair with a string")
+	b = append(b, "# [0][6] - a pair with a string without whitespace")
+	b = append(b, "# [0][7] - a pair with a string whitespace on the right")
+	b = append(b, "# [0][8] - another blank line")
+	b = append(b, "# [0][9] - a pair with a long name and another null value")
+	b = append(b, "# [0][10] - yet another blank line")
+	b = append(b, "# [0][11] - a pair with a multi line string")
+	b = append(b, "# [0][12] - a pair with a numeric")
+	b = append(b, "# [0][13] - a pair with a numeric without whitespace")
+	b = append(b, "# [0][14] - a pair with a numeric with whitespace on the right")
+	b = append(b, "# [0][15] - a pair with a numeric with whitespace on the left")
 	b = append(b, "#")
 	b = append(b, "")
 
@@ -101,11 +107,21 @@ func testNewFile01(t *testing.T, parser hcl.HclParser) {
 
 	el_num++
 	el = elements[el_num]
+	expect(t, el.Pair().Value(), "test5")
+	expect(t, el.Value(), "A \\\"simple\\\" string")
+
+	el_num++
+	el = elements[el_num]
+	expect(t, el.Pair().Value(), "test6")
+	expect(t, el.Value(), "A \\\"simple\\\" string")
+
+	el_num++
+	el = elements[el_num]
 	expect(t, el.Value(), "\n")
 
 	el_num++
 	el = elements[el_num]
-	expect(t, el.Pair().Value(), "test5_long_name")
+	expect(t, el.Pair().Value(), "test7_long_name")
 	expect(t, el.Value(), "null")
 
 	el_num++
@@ -114,6 +130,30 @@ func testNewFile01(t *testing.T, parser hcl.HclParser) {
 
 	el_num++
 	el = elements[el_num]
-	expect(t, el.Pair().Value(), "test6")
+	expect(t, el.Pair().Value(), "test8")
 	expect(t, el.Value(), "This is a multi-line string.\n")
+
+	el_num++
+	el = elements[el_num]
+	expect(t, el.Value(), "\n")
+
+	el_num++
+	el = elements[el_num]
+	expect(t, el.Pair().Value(), "test9")
+	expect(t, el.Value(), "1")
+
+	el_num++
+	el = elements[el_num]
+	expect(t, el.Pair().Value(), "test10")
+	expect(t, el.Value(), "2")
+
+	el_num++
+	el = elements[el_num]
+	expect(t, el.Pair().Value(), "test11")
+	expect(t, el.Value(), "3")
+
+	el_num++
+	el = elements[el_num]
+	expect(t, el.Pair().Value(), "test12")
+	expect(t, el.Value(), "4")
 }
--- tests/01/01.tfvars
+++ tests/01/01.tfvars
@@ -2,28 +2,41 @@
 # lines, null values, and multi-line strings. The parser should be able to
 # handle all of these elements correctly and produce the expected output.
 #
-# This file should have 10 elements.
+# This file should have 17 elements.
 #
 # [0]    - the file block
 # [0][0] - the multi-line comment
-# [0][1] - a pair with a null value
-# [0][2] - a pair with a null and without whitespace around the equals sign
-# [0][3] - a pair with a null and whitespace on right of equals sign
+# [0][1] - a pair with a null
+# [0][2] - a pair with a null without whitespace
+# [0][3] - a pair with a null with whitespace on the right
 # [0][4] - a blank line
-# [0][5] - a pair where the value is a string
-# [0][6] - another blank line
-# [0][7] - a pair with a long name and another null value
-# [0][8] - yet another blank line
-# [0][9] - a pair with a multi line string
+# [0][5] - a pair with a string
+# [0][6] - a pair with a string without whitespace
+# [0][7] - a pair with a string whitespace on the right
+# [0][8] - another blank line
+# [0][9] - a pair with a long name and another null value
+# [0][10] - yet another blank line
+# [0][11] - a pair with a multi line string
+# [0][12] - a pair with a numeric
+# [0][13] - a pair with a numeric without whitespace
+# [0][14] - a pair with a numeric with whitespace on the right
+# [0][15] - a pair with a numeric with whitespace on the left
 #
 test1 = null
 test2=null
 test3= null
 
 test4 = "A \"simple\" string"
+test5="A \"simple\" string"
+test6= "A \"simple\" string"
 
-test5_long_name = null
+test7_long_name = null
 
-test6 = <<EOT
+test8 = <<EOT
 This is a multi-line string.
 EOT
+
+test9 = 1
+test10=2
+test11= 3
+test12 =4
